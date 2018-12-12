445c4769a0c92a33cc5ca5e5dfe7b62e895a31d6
Oguz Bilgic <fisyonet@gmail.com>
2018-12-12 03:43:57 -0800

    Serialize blocks to file system

--- main.cr
+++ main.cr
@@ -6,11 +6,22 @@ port = Random.rand(1000) + 4000
 blockchain = Blockchain.new
 network = Network.new blockchain, "http://localhost:#{port}", ARGV.first?
 
-spawn do
-  start_web! port, network, blockchain
+Dir.open(".blocks/").each_child do |filename|
+  file = File.read ".blocks/#{filename}"
+  block = Block.from_json file
+  blockchain.add_relayed_block block
+end
+
+blockchain.on_block do |block|
+  Dir.mkdir ".blocks" unless File.exists? ".blocks"
+  File.write ".blocks/#{block.hash}", block.to_json
 end
 
 network.download_chain
+
+spawn do
+  start_web! port, network, blockchain
+end
 
 spawn do
   blockchain.work!
