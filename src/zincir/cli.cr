73df97c2a895d48c9bf31c98e2a9e440e90f480b
Oguz Bilgic <fisyonet@gmail.com>
2018-12-16 02:33:53 -0800

    Move Cli::Options in to cli.cr

--- src/zincir/cli.cr
+++ src/zincir/cli.cr
@@ -1,9 +1,16 @@
 require "option_parser"
 
-require "./cli/*"
-
 module Zincir
   module Cli
+    class Options
+      property seed_ip = Network::TESTNET_SEED_HOST
+      property host_ip : String? = nil
+      property port : Int32? = nil
+      property? mine = false
+      property? web = false
+      property? local = false
+    end
+
     def self.run!(args : Array(String))
       options = Options.new
 
