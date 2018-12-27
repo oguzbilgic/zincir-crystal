90e3c76bdc607846f0c827fa82c432c59ba8eb35
Oguz Bilgic <fisyonet@gmail.com>
2018-12-27 16:49:24 +0300

    Reorganize Blockchain::Tree#add_chain

--- src/zincir/blockchain/tree.cr
+++ src/zincir/blockchain/tree.cr
@@ -5,13 +5,17 @@ module Zincir
   class Blockchain::Tree < Blockchain
     private class Chain
       property parent : Chain? = nil
+      property branches = [] of UUID
 
       JSON.mapping(
         branches: {type: ::Array(UUID), setter: false},
         block: {type: Block, setter: false},
       )
 
-      def initialize(@block : Block, @branches)
+      def initialize(@block : Block, @parent = nil, new_branch? = false)
+        @branches = @parent.not_nil!.branches if @parent
+
+        @branches << UUID.random if new_branch?
       end
 
       def >(chain)
@@ -22,7 +26,7 @@ module Zincir
     # Creates a blockchain with the first block
     def initialize
       @orphans = [] of Block
-      @genesis = Chain.new Block.first, [UUID.random]
+      @genesis = Chain.new Block.first, new_branch?: true
       @chains_by_hash = {@genesis.block.hash => @genesis} of String => Chain
       @chains_by_index = {0 => [@genesis]}
       @tips = [@genesis]
@@ -30,7 +34,17 @@ module Zincir
 
     # Returns the last `Block` in the blockchain
     def last
-      highest_chain.not_nil!.block
+      @tips.max.not_nil!.block
+    end
+
+    # TODO: This shouldn't be exposed, currently for debugging purposes
+    def tips
+      @tips
+    end
+
+    # Queues the *block* to be added to the blockchain
+    def queue_block(block : Block)
+      add_chain block
     end
 
     # Returns the `Block` at *index*
@@ -40,6 +54,7 @@ module Zincir
     # easier way to find the block
     def block_at(index)
       chains = @chains_by_index[index].map do |chain|
+        highest_chain = @tips.max
         intersection = (chain.branches & highest_chain.branches)
         difference = (highest_chain.branches + chain.branches - intersection).uniq
         count = intersection.size - difference.size
@@ -48,21 +63,6 @@ module Zincir
       end
 
       chains.sort_by(&.["count"]).last["chain"].block
-    end
-
-    # Queues the *block* to be added to the blockchain
-    def queue_block(block : Block)
-      return if @chains_by_hash[block.hash]?
-
-      add_chain block
-    end
-
-    private def highest_chain
-      @tips.max
-    end
-
-    def tips
-      @tips
     end
 
     private def check_orphans(parent : Block)
@@ -77,55 +77,51 @@ module Zincir
     end
 
     private def add_chain(block : Block)
+      # Check if the block is already in the chain
+      return if @chains_by_hash[block.hash]?
+
       unless block.valid?
         raise BlockNotAdded.new "Invalid block at index #{block.index}"
       end
 
       parent = @chains_by_hash[block.previous_hash]?
 
-      if parent
-        if !parent.parent && parent != @genesis
-          puts "Orphan Parent #{block}"
-          @orphans << block
-          return
-        end
-
-        if parent.block.index + 1 != block.index
-          raise BlockNotAdded.new "Index is wrong #{block}"
-        end
-
-        if parent.block.timestamp > block.timestamp
-          raise BlockNotAdded.new "Time is wrong #{block}"
-        end
-
-        if block.difficulty != next_difficulty_at parent.block
-          raise BlockNotAdded.new "Difficulty is wrong #{block}"
-        end
-
-        if @tips.includes? parent
-          chain = Chain.new block, parent.branches
-          chain.parent = parent
-          @tips.delete parent
-        else
-          chain = Chain.new block, parent.branches + [UUID.random]
-          chain.parent = parent
-        end
-
-        @tips << chain
-        block_added block
-
-        check_orphans block
-
-        @chains_by_hash[chain.block.hash] = chain
-        if @chains_by_index[block.index]?
-          @chains_by_index[block.index] << chain
-        else
-          @chains_by_index[block.index] = [chain]
-        end
-      else
+      # Check if the block is orphan
+      unless parent
         puts "Orphan #{block}"
         @orphans << block
+        return
       end
+
+      if parent.block.index + 1 != block.index
+        raise BlockNotAdded.new "Index is wrong #{block}"
+      end
+
+      if parent.block.timestamp > block.timestamp
+        raise BlockNotAdded.new "Time is wrong #{block}"
+      end
+
+      if block.difficulty != next_difficulty_at parent.block
+        raise BlockNotAdded.new "Difficulty is wrong #{block}"
+      end
+
+      if @tips.includes? parent
+        chain = Chain.new block, parent
+        @tips.delete parent
+      else
+        chain = Chain.new block, parent, true
+      end
+
+      @chains_by_hash[chain.block.hash] = chain
+      if @chains_by_index[block.index]?
+        @chains_by_index[block.index] << chain
+      else
+        @chains_by_index[block.index] = [chain]
+      end
+
+      @tips << chain
+      block_added block
+      check_orphans block
     end
   end
 end
